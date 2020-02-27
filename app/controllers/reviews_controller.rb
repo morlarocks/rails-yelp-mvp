248c3634dec8359caa0965778d8cb03a10d30464
Birgit Klink <birgit@morla.rocks>
2020-02-27 08:47:52 +0800

    Challenge 2 almost done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  before_action :set_restaurant, except: [:destroy, :create]
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    if @review.save
+      redirect_to restaurant_path(@restaurant.id)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating, :restaurant_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,42 @@
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '+44-342-342',
+    category: 'chinese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '+1-342-342',
+    category: 'italian'
+  },
+  {
+    name: 'Zur Brezn',
+    address: 'Leopoldstraße München',
+    phone_number: '+49-342-342',
+    category: 'belgian'
+  },
+  {
+    name: 'Secret Spot',
+    address: 'Canggu',
+    phone_number: '+62-342-342',
+    category: 'japanese'
+  },
+  {
+    name: 'Nenillo',
+    address: 'Chlodwigplatz Köln',
+    phone_number: '+49-342-342',
+    category: 'italian'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
